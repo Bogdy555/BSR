70cad001d275c44b5036180ec181dcac3e3ddd99
Bogdy555 <florescubogdan555@gmail.com>
2023-04-03 03:49:52 +0300

    First deadline commit!
    This is the version that will be submitted for the first deadline.

--- Software Rasterizer/Sources/EntryPoint.cpp
+++ Software Rasterizer/Sources/EntryPoint.cpp
@@ -2,7 +2,17 @@
 
 
 
-int main()
+int WINAPI wWinMain(_In_ HINSTANCE _hInstance, _In_opt_ HINSTANCE _hPrevInstance, _In_ LPWSTR _CmdLine, _In_ int _ShowCmd)
 {
+	Rasterizer::Model _Model;
+
+	if (!_Model.Load(L".\\Cubes.wfobj"))
+	{
+		std::wcout << L"Could not load the 3D model!\n";
+		return -1;
+	}
+
+	std::wcout << L"The 3D model was loaded.\n";
+
 	return 0;
 }
