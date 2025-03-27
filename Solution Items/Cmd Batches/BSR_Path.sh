c841dd4872193f0c5f6707ad023c44b93c62bc0a
Bogdy555 <florescubogdan555@gmail.com>
2025-03-28 01:12:13 +0200

    Added execution flag to path script file!

--- Solution Items/Cmd Batches/BSR_Path.sh
+++ Solution Items/Cmd Batches/BSR_Path.sh
@@ -8,5 +8,6 @@ chmod +x "./BSR/Cmd Batches/BSR_Build_Debug_x64.sh"
 chmod +x "./BSR/Cmd Batches/BSR_Build_Debug_x86.sh"
 chmod +x "./BSR/Cmd Batches/BSR_Build_Release_x64.sh"
 chmod +x "./BSR/Cmd Batches/BSR_Build_Release_x86.sh"
+chmod +x "./Solution Items/Cmd Batches/BSR_Path.sh"
 
 popd
