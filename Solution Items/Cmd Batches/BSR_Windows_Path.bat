eac24d29161a6f2f6003bcb8b3f92236e4ab03c3
Bogdy555 <florescubogdan555@gmail.com>
2025-07-02 04:43:30 +0300

    Removed old AVR build system!

--- Solution Items/Cmd Batches/BSR_Windows_Path.bat
+++ Solution Items/Cmd Batches/BSR_Windows_Path.bat
@@ -2,12 +2,6 @@
 
 echo on
 
-if "%SolutionDir%" == "" set Path=D:\Aplicatii\avr8-gnu-toolchain-win32_x86_64\bin;D:\Aplicatii\avrdude;%Path%
-
-@echo off
-
-echo on
-
 if "%SolutionDir%" == "" set SolutionDir=%USERPROFILE%\source\repos\BSR\
 
 @echo off
